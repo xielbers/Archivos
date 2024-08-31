9353f81e0b4730b1d3b5d7fa669e52d0b48244b2
xielbers <salvadorrodrig568@gmail.com>
2024-08-31 18:04:20 -0300

    31/8

---
+++ Archivos/Scripts/evade.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KinetPhoenix/evade-script/main/evade.lua"))()
---
+++ index.js
@@ -0,0 +1,9 @@
+const archivos = {
+    'blox-fruits': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()'
+    'evade': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/KinetPhoenix/evade-script/main/evade.lua"))()'
+};
+
+function copy(archivo) {
+    navigator.clipboard.writeText(archivos[archivo]);
+    alert("Copiado");
+}
