f752d09114a3b0bfe6d2da60df639440447f06a4
xielbers <salvadorrodrig568@gmail.com>
2024-12-23 14:44:31 -0300

    23/12

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const archivos = {
-    'blox-fruits': 'loadstring(game:HttpGet("https://xielbers.github.io/Archivos/Scripts/blox-fruits.lua"))()',
-    'evade': 'loadstring(game:HttpGet("https://xielbers.github.io/Archivos/Scripts/evade.lua"))()'
+    'blox-fruits': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/Archivos/refs/heads/main/Scripts/blox-fruits.lua"))()',
+    'evade': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/Archivos/refs/heads/main/Scripts/evade.lua"))()'
 };
 
 function copy(archivo) {
