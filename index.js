fcef221fc9658f912ce46e0c62e3b7223e5324e0
xielbers <salvadorrodrig568@gmail.com>
2024-12-24 10:37:35 -0300

    24/12

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const archivos = {
     'blox-fruits_redz-hub': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blox-fruits_redz-hub.lua"))()',
     'blox-fruits_hutao': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blox-fruits_hutao.lua"))()',
-    'evade': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/evade.lua"))()'
+    'evade': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/evade.lua"))()',
+    'universal': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/universal.lua"))()',
 };
 
 function copy(archivo) {
