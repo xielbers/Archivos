65b829ca803c2d53feab9ef84d06e5bf0b29256a
xielbers <salvadorrodrig568@gmail.com>
2024-12-31 19:30:47 -0300

    31/12

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const archivos = {
     'blox-fruits_redz-hub': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blox-fruits_redz-hub.lua"))()',
     'blox-fruits_hutao': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blox-fruits_hutao.lua"))()',
     'evade': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/evade.lua"))()',
+    'evade-key': 'KINGKEYar6sD4TxdwAygGFmMeXR3HpVvJWnf7cjYL58btNuqUzCPSZBKh',
     'universal': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/universal.lua"))()',
+    'universal-key': 'UCrvd63k',
     'jailbreak': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/jailbreak.lua"))()',
 };
 
