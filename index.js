e0fb1246c073e16966ea2b5f71c203cb85b0a6a8
xielbers <salvadorrodrig568@gmail.com>
2025-02-15 18:00:47 -0300

    15

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const archivos = {
     'blox-fruits_blacktrap': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blox-fruits_blacktrap.lua"))()',
     'blox-fruits_redz-hub': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blox-fruits_redz-hub.lua"))()',
+    'blox-fruits_zenith-hub': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blox-fruits_zenith-hub.lua"))()',
     'evade': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/evade.lua"))()',
     'blade-ball': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blade-ball.lua"))()',
     'evade-key': 'KINGKEYar6sD4TxdwAygGFmMeXR3HpVvJWnf7cjYL58btNuqUzCPSZBKh',
