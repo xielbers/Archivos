4a00127ae2da1cdfb12dc6b488e301f0182f46c7
xielbers <salvadorrodrig568@gmail.com>
2025-03-09 10:42:59 -0300

    Pls donate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const archivos = {
     'evade': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/evade.lua"))()',
     'blade-ball': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/blade-ball.lua"))()',
     'evade-key': 'KINGKEYar6sD4TxdwAygGFmMeXR3HpVvJWnf7cjYL58btNuqUzCPSZBKh',
-    'pls-donate' 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/pls_donate.lua"))()',
+    'pls-donate': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/pls_donate.lua"))()',
     'universal': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/universal.lua"))()',
     'universal-key': 'UCrvd63k',
     'jailbreak': 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/jailbreak.lua"))()',
